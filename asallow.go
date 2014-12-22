1b91d0212db50f761807764a286e925bb0d666a5
Wim <wim@42.be>
2014-12-22 12:47:00 +0100

    add country support + refactoring

--- asallow.go
+++ asallow.go
@@ -12,21 +12,38 @@ import (
 	"os"
 	"os/exec"
 	"strings"
+	"sync"
 	"time"
 )
 
 const PREFIX_URI = "https://stat.ripe.net/data/announced-prefixes/data.json?resource="
+const COUNTRY_URI = "https://stat.ripe.net/data/country-resource-list/data.json?resource="
+const PREFIX = 0
+const COUNTRY = 1
 
-var ipset_count int = 0
-var ipset_string string = ""
-var ipset6_string string = ""
+type ipsetInfo struct {
+	sync.Mutex
+	v4count int
+	v6count int
+	v4      string
+	v6      string
+}
 
 type config struct {
 	Main struct {
-		Allow []string
-		ASN   []string
+		Allow   []string
+		ASN     []string
+		Country []string
 	}
 }
+
+type resourceInfo struct {
+	id  int
+	uri string
+	cfg []string
+}
+
+var ipset ipsetInfo
 
 func readconfig(cfgfile string) config {
 	var cfg config
@@ -41,9 +58,8 @@ func readconfig(cfgfile string) config {
 	return cfg
 }
 
-func getAS(ASnumber string) []byte {
-	fmt.Println("fetching ASN: " + ASnumber)
-	resp, err := http.Get(PREFIX_URI + ASnumber)
+func getURI(uri string) []byte {
+	resp, err := http.Get(uri)
 	if err != nil {
 		log.Fatal("site not available")
 	}
@@ -74,7 +90,7 @@ func doipset() {
 	ipset_footer += "swap AS_allow6 AS_allow_swap6\n"
 	ipset_footer += "destroy AS_allow_swap\n"
 	ipset_footer += "destroy AS_allow_swap6\n"
-	ipset_string = ipset_header + ipset_string + ipset_footer
+	ipset_string := ipset_header + ipset.v4 + ipset.v6 + ipset_footer
 	cmd := exec.Command("ipset", "-!", "restore")
 	cmd.Stdin = strings.NewReader(ipset_string)
 	out, err := cmd.CombinedOutput()
@@ -84,34 +100,70 @@ func doipset() {
 	}
 }
 
-func parseBody(body []byte, ASnumber string, sc chan string) {
-	ipset_string := ""
-	dec := json.NewDecoder(strings.NewReader(string(body)))
-	var mapstring map[string]interface{}
-	if err := dec.Decode(&mapstring); err != nil {
-		log.Fatal(err)
+func parseCountry(mapstring map[string]interface{}) []string {
+	var array []string
+	datamap := mapstring["data"]
+	mapstring = datamap.(map[string]interface{})
+	resources := mapstring["resources"]
+	mapstring = resources.(map[string]interface{})
+	ipv6 := mapstring["ipv6"]
+	ipv6_array := ipv6.([]interface{})
+	ipv4 := mapstring["ipv4"]
+	ipv4_array := ipv4.([]interface{})
+	for _, prefix_element := range append(ipv4_array, ipv6_array...) {
+		array = append(array, prefix_element.(string))
 	}
+	return array
+}
+
+func parseASN(mapstring map[string]interface{}) []string {
+	var array []string
 	datamap := mapstring["data"]
 	mapstring = datamap.(map[string]interface{})
 	prefixes := mapstring["prefixes"]
 	prefixes_array := prefixes.([]interface{})
 	for _, prefix_element := range prefixes_array {
 		mapstring = prefix_element.(map[string]interface{})
-		prefix := mapstring["prefix"].(string)
+		array = append(array, mapstring["prefix"].(string))
+	}
+	return array
+}
+
+func parseBody(body []byte, id int, comment string, sc chan string) {
+	var array []string
+	var comment_prefix string
+	var mapstring map[string]interface{}
+
+	dec := json.NewDecoder(strings.NewReader(string(body)))
+	if err := dec.Decode(&mapstring); err != nil {
+		log.Fatal(err)
+	}
+	if id == PREFIX {
+		array = parseASN(mapstring)
+		comment_prefix = "AS"
+	} else {
+		array = parseCountry(mapstring)
+	}
+	for _, prefix := range array {
 		ip := isIpOrCidr(prefix) // input validation
 		if ip != nil {           // it really is an IP
 			if ip.To4() != nil { // is it IPv4
-				ipset_string += "add AS_allow_swap " + prefix + " comment AS" + ASnumber + "\n"
+				ipset.Lock()
+				ipset.v4 += "add AS_allow_swap " + prefix + " comment " + comment_prefix + comment + "\n"
+				ipset.v4count += 1
+				ipset.Unlock()
 			} else { // ipv6
-				ipset_string += "add AS_allow_swap6 " + prefix + " comment AS" + ASnumber + "\n"
+				ipset.Lock()
+				ipset.v6 += "add AS_allow_swap6 " + prefix + " comment " + comment_prefix + comment + "\n"
+				ipset.v6count += 1
+				ipset.Unlock()
 			}
-			ipset_count += 1
 		} else {
-			log.Println("not an ip (range): " + prefix)
+			log.Println("not an ip (range): " + prefix + comment_prefix + " " + comment)
 		}
 	}
-	//fmt.Println("starting thread for: "+ASnumber)
-	sc <- ipset_string
+	//fmt.Println("starting thread for: "+comment_prefix)
+	sc <- "done"
 }
 
 func addAllowed(allowed []string) {
@@ -119,11 +171,12 @@ func addAllowed(allowed []string) {
 		ip := isIpOrCidr(el)
 		if ip != nil { //really an IP
 			if ip.To4() != nil {
-				ipset_string += "add AS_allow_swap " + el + " comment \"read from asallow.conf\"\n"
+				ipset.v4 += "add AS_allow_swap " + el + " comment \"read from asallow.conf\"\n"
+				ipset.v4count += 1
 			} else {
-				ipset_string += "add AS_allow_swap6 " + el + " comment \"read from asallow.conf\"\n"
+				ipset.v6 += "add AS_allow_swap6 " + el + " comment \"read from asallow.conf\"\n"
+				ipset.v6count += 1
 			}
-			ipset_count += 1
 		} else {
 			log.Println("not an ip (range): " + el)
 		}
@@ -136,23 +189,40 @@ func main() {
 	}
 	cfgfile := flag.String("conf", "asallow.conf", "a valid config file")
 	flag.Parse()
+
+	counter := 0
 	sc := make(chan string)
+
+	// parse the config
 	cfg := readconfig(*cfgfile)
+	resources := []resourceInfo{
+		{PREFIX, PREFIX_URI, cfg.Main.ASN},
+		{COUNTRY, COUNTRY_URI, cfg.Main.Country},
+	}
+
+	// add always the static entries
 	addAllowed(cfg.Main.Allow)
-	for i, ASN := range cfg.Main.ASN {
-		if i > 0 && i%2 == 0 { // max 2 rqs
-			time.Sleep(time.Second)
-		}
-		go func(ASN string) {
-			body := getAS(ASN)
-			go parseBody(body, ASN, sc)
-		}(ASN)
-	}
-	for range cfg.Main.ASN {
-		ipset_string += <-sc
-	}
 	doipset()
 
-	fmt.Printf("%v subnets added\n", ipset_count)
+	for _, resource := range resources {
+		for i, uri_id := range resource.cfg {
+			go func(uri_id string, resource resourceInfo) {
+				if counter > 0 && i%2 == 0 { // max 2 rqs
+					time.Sleep(time.Second)
+				}
+				counter += 1
+				body := getURI(resource.uri + uri_id)
+				go parseBody(body, resource.id, uri_id, sc)
+			}(uri_id, resource)
+		}
+	}
+
+	for range append(cfg.Main.Country, cfg.Main.ASN...) {
+		<-sc
+	}
+
+	doipset()
+
+	fmt.Printf("%v ipv4 / %v ipv6 subnets added\n", ipset.v4count, ipset.v6count)
 	fmt.Println("AS_allow and AS_allow6 ipset created/modified")
 }
